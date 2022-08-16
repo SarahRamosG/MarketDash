65c700c0222f6c1f4780c582e9a19913bf1713a3
Sarah Ramos <markektdash>
2022-08-15 23:48:28 -0400

    modificacion de clientes

--- src/app/pagos/pagos.page.ts
+++ src/app/pagos/pagos.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Pipe, PipeTransform} from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { CartService, Products } from '../services/cart.service';
 
 
 @Component({
@@ -11,22 +12,26 @@ import { DatePipe } from '@angular/common';
  
 })
 export class PagosPage implements OnInit {
+   cart = [];
   isModalOpen = false;
   date : Date = new Date ();
   pipe = new DatePipe('en-US');
   todayWithPipe = null;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private cartServices: CartService) { }
 
   ngOnInit() {
-
+      
       this.todayWithPipe = this.pipe.transform(Date.now(), 'dd/MM/yyyy');
+
+
     }
   
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
+
 }
 
 
