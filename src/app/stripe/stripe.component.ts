4d2741dc9ddbc73d4095fc4373af10f288bd9ce4
Sarah Ramos <markektdash>
2022-08-18 09:31:01 -0400

    ultimos cambios

--- src/app/stripe/stripe.component.ts
+++ src/app/stripe/stripe.component.ts
@@ -8,6 +8,7 @@ import { AppStoreService} from '../services/app-store.service';
 import { TouchSequence } from 'selenium-webdriver';
 import { Routes, RouterModule, Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-stripe',
   templateUrl: './stripe.component.html',
@@ -38,11 +39,11 @@ export class StripeComponent implements OnInit {
   //Vamos a pasar la llame publica y cuando hagamos eso esperaremos una respuesta
   async ngOnInit() {
 
-    this.inicializar();
 
     //this.monto  = parseInt(this.carrito.getTotal());
-    this.monto = this.getTotal();
+    this.monto = parseInt(this.getTotal());
     this.stripe = await loadStripe('pk_test_51LWPFkAlAGmCEgYAhmZCFDNt2eCT8bEnoXCmogrMGTH4w8iYaXrqtxeFvpFtycziovqqSPgHDxI69yhkQBLB5VBg00dcOoVaQd');
+    
    
     const elements = this.stripe.elements();
     const card = elements.create ('card');
@@ -52,34 +53,27 @@ export class StripeComponent implements OnInit {
     card.on('change', async (event)=>{
       const displayError = document.getElementById('card-errors');
       this.mensaje= event.error ? displayError.textContent = event.error.message: displayError.textContent = '';  
-      
-     /*const toast = await this.ToastController.create({
-        message: this.mensaje,
-        duration: 2000
-  
-      });
-  
-      toast.present();*/
       });
       
       const button = document.getElementById('button');
+
       button.addEventListener('click', async (event) => {
         event.preventDefault();
         const ownerInfo = {
           owner: {name : 'codexmaker'},
           //el dice que un peso es igual a 100 centavos por eso lo multiplica por 100
-          amount : 3000,
+          amount : 1000 * 100,
           //su documentacion pide que sea en minuscula
           currency: 'dop',
         };
-        
+
 
         //creando un recurso
         try{
-          const result = await this.stripe.createSource(card,ownerInfo);
+          const result = await this.stripe.createSource(card, ownerInfo);
           console.log(result);
         } catch (e){
-          console.log(e.message);
+          console.warn(e.message);
           
       
         }
@@ -94,9 +88,10 @@ export class StripeComponent implements OnInit {
       message: "Gracias por realizar su compra por MarketDash",
       buttons: ['Aceptar']
     });
-    await alert.present();
-    this.router.navigate(['/supermercados']); 
+    await alert.present(); 
+    this.router.navigate(['/supermercados']);
     return;
+  
   
   }
 
@@ -105,9 +100,6 @@ export class StripeComponent implements OnInit {
     return this.cart.reduce((i,j)=> i + j.precio * j.initialValue, 0);
   }
 
-  inicializar( ){
-    this.dataservice.getDocument("factura").subscribe(data=> this.factura1 = data);
-
-  }
+  
   }
 
